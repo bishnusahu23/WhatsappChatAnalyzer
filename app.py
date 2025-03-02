3c2c734175f3b6c0ff6d509c37c42801c920f93b
bks <sahu.bishnukumari@gmail.com>
2025-03-02 20:53:40 +0530

    updated

--- app.py
+++ app.py
@@ -55,8 +55,6 @@ if uploaded_file is not None:
 
     # Preprocess Data
     df = helper.preprocess(data)
-    st.write(df.head())
-
 
     user_analysis = st.toggle("Analyze Specific User", value=False)
     selected_user = "Overall"
