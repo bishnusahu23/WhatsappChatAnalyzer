8d74fda477dfd5723ccaf1804052a10158772982
bks <sahu.bishnukumari@gmail.com>
2025-03-04 22:35:40 +0530

    updated

--- helper.py
+++ helper.py
@@ -121,7 +121,7 @@ def create_wordcloud(user, df):
     mask_size = (450, 450)  # Define the size of the word cloud
     mask = Image.new("L", mask_size, 255)  # Create a white canvas
     draw = ImageDraw.Draw(mask)
-    draw.ellipse((20, 70, 450, 450), fill=0)  # Draw an ellipse (adjust for best shape)
+    draw.ellipse((20, 150, 450, 450), fill=0)  # Draw an ellipse (adjust for best shape)
     mask = np.array(mask)
 
     wc = WordCloud(min_font_size=7,width=450, height=450,mode='RGBA', background_color='black',mask=mask,max_words=150).generate(" ".join(words))
